f0a910295587b1d4a956cdb13e95c41e5f435be6
mateogall0 <mateogesede@gmail.com>
2025-09-10 17:50:01 -0300

    tests: assert that errors are catched

--- tests/server/socket/test_raw.py
+++ tests/server/socket/test_raw.py
@@ -3,18 +3,20 @@ import pytest
 from tests.fixtures.socket import example_server
 
 
-@pytest.mark.parametrize("target,body,expected",
+@pytest.mark.parametrize("method_str,target,body,expected",
                          [
-                             ('/get-1515', '', b'1515'),
-                             ('/get-pow', 5, b'25'),
-                             ('/get-pow', 0, b'0'),
-                             ('/get-pow', '10', b'100'),
-                             ('/get-pow', -10, b'100'),
-                             ('/get-pow', 'something', b'error'),
+                             ('GET', '/get-1515', '', b'1515'),
+                             ('GET', '/get-pow', 5, b'25'),
+                             ('GET', '/get-pow', 0, b'0'),
+                             ('GET', '/get-pow', '10', b'100'),
+                             ('GET', '/get-pow', -10, b'100'),
+                             ('GET', '/get-pow', 'something', b'error'),
+                             ('GET', '/get-notnonexistent', '', b'404 Resource not found'),
+                             ('POST', '/get-1515', '', b'405 Method not allowed'),
                          ])
-def test_server_socket(example_server, target, body, expected):
+def test_server_socket(example_server, method_str, target, body, expected):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("127.0.0.1", example_server))
-        s.sendall(f"GET {target}\r\n\r\n{body}".encode())
+        s.sendall(f"{method_str} {target}\r\n\r\n{body}".encode())
         data = s.recv(1024)
         assert data == expected
