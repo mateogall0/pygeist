df178ef804336d8ee088d9d37c19c4695c0c4134
mateogall0 <mateogesede@gmail.com>
2025-09-12 19:47:43 -0300

    cd: manifest

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="pygeist",
     version="0.1.0",
-    packages=find_packages(),
+    packages=[],  # empty core package
     python_requires=">=3.8",
     install_requires=[],
+    extras_require={
+        "server": ["zeitgeist_server"],  # treat this as extra
+    },
+    include_package_data=True,
+    description="Pygeist server package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
