c992ca8389f665b7176edd532234ca3c6f1fac4f
mateogall0 <mateogesede@gmail.com>
2025-11-06 13:50:23 -0300

    chore: setup version now retrieved from __init__

--- pygeist/__init__.py
+++ pygeist/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from .zeitgeist import ZeitgeistAPI
 from .router import Router
--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, find_packages
+import pathlib
+import re
 
+
+init_file = pathlib.Path("pygeist/__init__.py").read_text()
+
+
+version_match = re.search(r'__version__\s*=\s*"([^"]+)"', init_file)
+if not version_match:
+    raise RuntimeError("Unable to find version string in pygeist/__init__.py.")
+
+v = version_match.group(1)
 
 setup(
     name="pygeist",
-    version="0.3.1",
+    version=v,
     packages=find_packages(include=["pygeist*"]),
     python_requires=">=3.10",
     install_requires=open("requirements.txt").read().splitlines(),
