6b6560ed97539d424086f9eec8eee3e6d2b49074
mateogall0 <mateogesede@gmail.com>
2025-12-10 13:23:58 -0300

    chore: move init from abstract dependency to implementation

--- pygeist/abstract/dependency.py
+++ pygeist/abstract/dependency.py
@@ -3,11 +3,6 @@ from typing import Callable
 
 
 class Dependency(ABC):
-    def __init__(self,
-                 dependency: Callable,
-                 ) -> None:
-        self.dependency = dependency
-
     @abstractmethod
     async def call_depend(self,
                           args: list = [],
--- pygeist/dependency.py
+++ pygeist/dependency.py
@@ -1,7 +1,13 @@
 from pygeist.abstract.dependency import Dependency
+from typing import Callable
 
 
 class Depends(Dependency):
+    def __init__(self,
+             dependency: Callable,
+             ) -> None:
+        self.dependency = dependency
+
     async def call_depend(self,
                           args: list = [],
                           kwargs: dict = {},
